cb944c02296158980fa41954ead69bf44ecff341
jarcane <jarcane@users.noreply.github.com>
2014-07-25 13:59:58 +0300

    Removed unnecessary files and added licensing info

--- libbluebox.py
+++ libbluebox.py
@@ -3,6 +3,20 @@ libbluebox.py - BlueBox core library
 
 This file contains the basic class object BlueBox and its subroutines.
 
+Copyright 2014 by John S. Berry III.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import libtcodpy as libtcod
